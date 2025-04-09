d157bc575675427645ff82615634045874e6cb8b
micah-olivas <micah5051olivas@gmail.com>
2025-04-09 10:33:17 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="beamkit",
+    version="0.1.0",
+    description="A toolkit for biophysical and evolutionary association modeling",
+    author="Micah Olivas",
+    author_email="micah5051olivas@gmail.com",
+    license="MIT",
+    packages=find_packages(exclude=["tests*", "examples*", "docs*"]),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "biopython"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.7"
+)
