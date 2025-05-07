7301881fd8a4fad4b0e2992caf2d370e4a34df0d
Micah B Olivas <51717162+micah-olivas@users.noreply.github.com>
2025-05-07 16:18:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="beamkit",
+    name="beak",
     version="0.1.0",
     description="A toolkit for biophysical and evolutionary association modeling",
     author="Micah Olivas",
