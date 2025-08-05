2c8128203dce6b120a1ca63e425461f12c0a01a7
micah-olivas <micah5051olivas@gmail.com>
2025-08-05 10:31:55 -0700

    Added new remote functions

--- src/beak/remote/__init__.py
+++ src/beak/remote/__init__.py
@@ -4,6 +4,10 @@ Remote computing utilities for SSH connections and remote analysis
 
 from .utils import (
     authenticate,
+    get_current_user,
+    is_authenticated,
+    clear_authentication,
+    show_auth_status,
     get_pw,
     sopen,
     ssend,
@@ -11,7 +15,17 @@ from .utils import (
     scp_to_remote,
     scp_from_remote,
     nest,
-    search
+    search,
+    status,
+    retrieve_results,
+    align,
+    compute_tree,
+    taxonomy,
+    check_remote_process,
+    list_databases,
+    projects,
+    debug_projects,
+    simple_projects_test
 )
 
 from .interactive import (
