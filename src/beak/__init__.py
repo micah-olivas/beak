728a9eec5646852501ea8a1645a656404407910d
micah-olivas <micah5051olivas@gmail.com>
2025-11-11 19:16:07 -0800

    Add pipelines to package __init__.py

--- src/beak/__init__.py
+++ src/beak/__init__.py
@@ -1,14 +1,9 @@
-"""
-Beak - A toolkit for biophysical and evolutionary association modeling
-"""
+"""BEAK - Biophysical and Evolutionary Associations Kit"""
 
-from .viz import (
-    plot_seq_length_hist,
-    plot_sequence_logo, 
-    plot_pssm_single_site_taxa_heatmap,
-    plot_conservation_with_sequence,
-    interactive_pssm_heatmap
-)
+from . import remote
+from . import datasets
+from . import utils
 
-__version__ = "0.1.0"
-__author__ = "Micah Olivas"
+__version__ = '0.1.0'
+
+__all__ = ['remote', 'datasets', 'utils']
