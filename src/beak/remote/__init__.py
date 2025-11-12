9555f5f0881452992d54cab99897227f791b4585
micah-olivas <micah5051olivas@gmail.com>
2025-11-11 19:18:32 -0800

    Refactor remote/__init__.py for fabric implementation

--- src/beak/remote/__init__.py
+++ src/beak/remote/__init__.py
@@ -1,35 +1,5 @@
-"""
-Remote computing utilities for SSH connections and remote analysis
-"""
+from .search import MMseqsSearch
+from .taxonomy import MMseqsTaxonomy
+from .pipeline import Pipeline
 
-from .utils import (
-    authenticate,
-    get_current_user,
-    is_authenticated,
-    clear_authentication,
-    show_auth_status,
-    get_pw,
-    sopen,
-    ssend,
-    make_temp_dir,
-    scp_to_remote,
-    scp_from_remote,
-    nest,
-    search,
-    status,
-    retrieve_results,
-    align,
-    compute_tree,
-    taxonomy,
-    check_remote_process,
-    list_local_projects,
-    list_databases,
-    projects,
-    debug_projects,
-    simple_projects_test
-)
-
-from .interactive import (
-    InteractiveRemoteSession,
-    connect_remote
-)
+__all__ = ['MMseqsSearch', 'MMseqsTaxonomy', 'Pipeline']
