998066b2418523f853c556760b837e7b326b31d8
micah-olivas <micah5051olivas@gmail.com>
2026-01-22 14:42:14 -0800

    Update main init

--- src/beak/__init__.py
+++ src/beak/__init__.py
@@ -3,7 +3,9 @@
 from . import remote
 from . import datasets
 from . import utils
+from . import alignments
+from . import viz
 
 __version__ = '0.1.0'
 
-__all__ = ['remote', 'datasets', 'utils']
+__all__ = ['remote', 'datasets', 'utils', 'alignments', 'viz']
